8b797685ce62ff725a9e390b4cc1fcfa6a535bf1
Jason White <jmw5598@gmail.com>
2019-11-10 18:12:20 -0500

    added services and repositories as dependencies, configured authentication

--- src/Auth.Api/Startup.cs
+++ src/Auth.Api/Startup.cs
@@ -10,6 +10,30 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+using Auth.Core.Interfaces;
+using Auth.Core.Models;
+using Auth.Infrastructure.Data;
+using Auth.Infrastructure.Services;
+using Auth.Infrastructure.Repositories;
 
 namespace Auth.Api
 {
@@ -26,6 +50,46 @@ namespace Auth.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options =>
+                options
+                    .UseNpgsql(Configuration["Database:ConnectionStrings:Default"])
+                    .UseSnakeCaseNamingConvention());
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            // Authentication
+            var authenticationSettingsSection = Configuration.GetSection("AuthenticationSettings");
+            services.Configure<AuthenticationSettings>(authenticationSettingsSection);
+
+            var authenticationSettings = authenticationSettingsSection.Get<AuthenticationSettings>();
+            var key = Encoding.ASCII.GetBytes(authenticationSettings.Secret);
+            
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options => 
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // Repositories
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+            // Services
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IPasswordHasher, BcryptPasswordHasher>();
+            services.AddScoped<ITokenService, JwtTokenService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +99,15 @@ namespace Auth.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
