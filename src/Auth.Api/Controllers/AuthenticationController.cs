3d36b5e27e15c8e57e87929af45315d4477b3996
Jason White <jmw5598@gmail.com>
2019-11-10 17:57:22 -0500

    added authentication controller with tokens, refrehs, and register endpoints

---
+++ src/Auth.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Auth.Core.Entities;
+using Auth.Core.Interfaces;
+using Auth.Core.Models;
+using Auth.Infrastructure.Data;
+using Auth.Web.Models;
+
+namespace Auth.Web.Controller
+{
+    [ApiController]
+    [Route("auth/")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly ITokenService _tokenService;
+        private readonly IPasswordHasher _hasher;
+        private readonly IUsersRepository _usersRepository;
+
+        public AuthenticationController(IAuthenticationService authenticationService, 
+            ITokenService tokenService, IPasswordHasher hasher, IUsersRepository usersRepository)
+        {
+            this._authenticationService = authenticationService;
+            this._hasher = hasher;
+            this._tokenService = tokenService;
+            this._usersRepository = usersRepository;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("tokens")]
+        public async Task<ActionResult<AuthenticatedUser>> AuthenticateUser([FromBody] Credentials credentials)
+        {
+            var authenticatedUser = await this._authenticationService.AuthenticateAsync(credentials);
+
+            if (authenticatedUser == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(authenticatedUser);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<object>> RegisterUser(UserBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            
+            var user = new User
+            {
+                Username = model.Username,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Password = await this._hasher.HashAsync(model.Password),
+                Role = Role.USER
+            };
+
+            this._usersRepository.Create(user);
+
+            return Ok("Created");
+        }            
+
+        [AllowAnonymous]
+        [HttpPost("refresh")]
+        public async Task<ActionResult<object>> RefreshToken(Token token)
+        {
+            var authenticatedUser = await this._authenticationService.RefreshAsync(token);
+
+            if (authenticatedUser == null)
+            {
+                return Unauthorized();
+            } 
+
+            return Ok(authenticatedUser);
+        }
+    }
+} 
